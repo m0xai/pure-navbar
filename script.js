e1ea976b6230c9e6799258eb785b5f314e654399
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-10-10 00:09:55 +0300

    Add menu items

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const hello = 'hello!'
+// Navbar Dropdown
+const navBarMoreBtn = document.getElementsByClassName('menu-link');
 
-document.inserAdjencedElement('beforebegin', hello)
+const menuLink = navBarMoreBtn[navBarMoreBtn.length - 1];
+
+function addIndicator(menuItem) {
+  const indicator = document.createElement('span');
+  indicator.textContent = '>';
+  menuItem.addEventListener('click', () => handleOnClick(indicator));
+  menuItem.appendChild(indicator);
+}
+
+function handleOnClick(indicator) {
+  indicator.classList.add('anim-menu-toggle-button');
+}
+
+addIndicator(menuLink);
