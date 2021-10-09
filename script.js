359c97f84d824b924a93c83f7ec3cad5acea1fb9
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-10-10 00:55:35 +0300

    Add Dropdown Maker functions

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 // Navbar Dropdown
 const navBarMoreBtn = document.getElementsByClassName('menu-link');
 
-const menuLink = navBarMoreBtn[navBarMoreBtn.length - 1];
+const menuLink = navBarMoreBtn[navBarMoreBtn.length - 3];
 
 function addIndicator(menuItem) {
   const indicator = document.createElement('span');
   indicator.textContent = '>';
-  menuItem.addEventListener('click', () => handleOnClick(indicator));
+  menuItem.addEventListener('click', () => handleOnClick(indicator, menuItem));
   menuItem.appendChild(indicator);
 }
 
-function handleOnClick(indicator) {
-  indicator.classList.add('anim-menu-toggle-button');
+function handleOnClick(indicator, menuItem) {
+  indicator.classList.toggle('anim-menu-toggle-button');
+  createSubNavDiv(menuItem);
 }
 
 addIndicator(menuLink);
+
+function createSubNav(itemCount) {
+  const itemsArr = [];
+  const itemPlaceHolders = ['Lorem', 'Ipsum', 'Dior', 'Sit', 'Amenc'];
+  for (let i = 0; i < itemCount; i++) {
+    const item = document.createElement('a');
+    item.textContent = itemPlaceHolders[Math.random * itemPlaceHolders.length];
+    item.href = '#';
+    itemsArr.push(item);
+  }
+  return itemsArr;
+}
+
+function createSubNavDiv(parent) {
+  const nav = document.createElement('nav');
+  nav.classList.add('dropdown-wrapper');
+  parent.appendChild(nav);
+  return nav;
+}
