8511be7f4f6d3e843e4c292c7312e1e91c25ab83
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-10-30 11:18:50 +0300

    Change style of navbar on mobile devices

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,27 +1,27 @@
 // Use this placeholder object to create nav elements
 const navItems = {
-  siteName: ['My Website', '/'],
-  home: ['Home Page', '/'],
-  about: ['About', '#'],
-  blog: ['Blog', '#'],
-  karierre: ['Karierre', '#'],
-  contact: ['Contact', '#'],
+  siteName: ["My Website", "/"],
+  home: ["Home Page", "/"],
+  about: ["About", "#"],
+  blog: ["Blog", "#"],
+  karierre: ["Karierre", "#"],
+  contact: ["Contact", "#"],
 };
 
 const subNavItems = {
   // data-attribute: ["innerText", "url"]
-  stepOne: ['Step One', '#'],
-  testingSubNav: ['Testing sub nav', '#'],
-  github: ['Github', 'https://github.com/m0xai'],
-  kerem: ['Kerem Zopcuk', 'https://keremz.de'],
+  stepOne: ["Step One", "#"],
+  testingSubNav: ["Testing sub nav", "#"],
+  github: ["Github", "https://github.com/m0xai"],
+  kerem: ["Kerem Zopcuk", "https://keremz.de"],
 };
 
 function createNavbar(parentKey, navItemsObj) {
-  createAndAttach(parentKey, 'nav', 'nav-wrapper', 'nav-wrapper');
-  createNavList('nav-wrapper', navItemsObj, 'parent');
+  createAndAttach(parentKey, "nav", "nav-wrapper", "nav-wrapper");
+  createNavList("nav-wrapper", navItemsObj, "parent");
 }
 
-createNavbar('header', navItems);
+createNavbar("header", navItems);
 
 // Create sub nav function takes parentKey, subItemsObj.
 // This function creates data id for every item .
@@ -29,35 +29,36 @@ createNavbar('header', navItems);
 //! Call this only if there should be a sub nav element
 // Subnavkey can be anything
 function createSubNav(parentKey, subItemsObj) {
-  createNavList(parentKey, subItemsObj, 'child');
+  createNavList(parentKey, subItemsObj, "child");
 }
 
-createSubNav('blog', subNavItems);
+createSubNav("blog", subNavItems);
+createSubNav("about", subNavItems);
 
 //* Helper Function: Create and attach an item to an parent element.
 function createAndAttach(parentKey, el, elKey, elClass) {
   const item = document.createElement(el);
   const parent = document.querySelector(`[data-key=${parentKey}]`);
-  item.setAttribute('data-key', elKey);
+  item.setAttribute("data-key", elKey);
   elClass ? item.classList.add(elClass) : null;
   parent.appendChild(item);
 }
 
 function addListener(el, sub) {
-  console.log('subdisp', sub.style.display);
-  el.addEventListener('click', () => {
-    console.log('hello');
+  console.log("subdisp", sub.style.display);
+  el.addEventListener("click", () => {
+    console.log("hello");
     toggleDisplay(el, sub);
   });
 }
 
 function toggleDisplay(el, sub) {
-  if (sub.style.display === 'none' || sub.style.display === '') {
-    sub.style.display = 'block';
-    el.style.backgroundColor = 'white';
+  if (sub.style.display === "none" || sub.style.display === "") {
+    sub.style.display = "block";
+    el.style.backgroundColor = "white";
   } else {
-    sub.style.display = 'none';
-    el.style.backgroundColor = 'unset';
+    sub.style.display = "none";
+    el.style.backgroundColor = "unset";
   }
 }
 
@@ -66,22 +67,27 @@ function toggleDisplay(el, sub) {
 //TODO: Divide this to new under functions like: createNavList_li
 function createNavList(parent, list, type) {
   const parentEl = getELFromKey(parent);
-  const ul = document.createElement('ul');
+  const ul = document.createElement("ul");
   // spare items of their type
-  if (type === 'parent') ul.classList.add('menu-list-wrapper');
-  if (type === 'child') {
-    parentEl.classList.add('has-subnav');
-    ul.classList.add('subnav-wrapper');
+  if (type === "parent") ul.classList.add("menu-list-wrapper");
+  if (type === "child") {
+    parentEl
+      .querySelector("a")
+      .querySelector("span")
+      .classList.add("has-subnav");
+    ul.classList.add("subnav-wrapper");
     addListener(parentEl, ul);
   }
   for (let prop in list) {
-    console.log('param: prop in for..in loop at createNavList');
-    const li = document.createElement('li');
-    li.classList.add('menu-link');
+    console.log("param: prop in for..in loop at createNavList");
+    const li = document.createElement("li");
+    li.classList.add("menu-link");
     li.dataset.key = prop;
-    const a = document.createElement('a');
-    a.innerText = list[prop][0];
+    const a = document.createElement("a");
     a.href = list[prop][1];
+    const span = document.createElement("span");
+    span.innerText = list[prop][0];
+    a.appendChild(span);
     li.appendChild(a);
     ul.appendChild(li);
   }
